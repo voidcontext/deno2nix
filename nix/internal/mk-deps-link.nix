a3818bbec3589c02008280d57227fc04726781a0
Amos Wenger <amoswenger@gmail.com>
2023-02-15 16:51:57 +0100

    Specify deno user agent

--- nix/internal/mk-deps-link.nix
+++ nix/internal/mk-deps-link.nix
@@ -1,37 +1,40 @@
-{
-  lib,
-  linkFarm,
-  writeText,
-  deno2nix,
-  ...
-}: let
+{ lib
+, linkFarm
+, writeText
+, deno2nix
+, ...
+}:
+let
   inherit (builtins) split elemAt fetchurl toJSON hashString baseNameOf;
   inherit (lib) flatten mapAttrsToList importJSON;
   inherit (lib.strings) sanitizeDerivationName;
   inherit (deno2nix.internal) artifactPath;
 in
-  lockfile: (
-    linkFarm "deps" (flatten (
-      mapAttrsToList
+lockfile: (
+  linkFarm "deps" (flatten (
+    mapAttrsToList
       (
-        url: sha256: let
+        url: sha256:
+        let
         in [
           {
             name = artifactPath url;
-            path = fetchurl {
-              inherit url sha256;
-              name = sanitizeDerivationName (baseNameOf url);
-            };
-          }
-          {
-            name = artifactPath url + ".metadata.json";
-            path = writeText "metadata.json" (toJSON {
-              inherit url;
-              headers = {};
-            });
-          }
-        ]
-      )
-      (importJSON lockfile).remote
-    ))
-  )
+            path = fetchurl
+              {
+                inherit url sha256;
+                name = sanitizeDerivationName (baseNameOf url);
+                curlOpts = '' -H "User-Agent: Deno/1.0" ''
+                  };
+              }
+              {
+                name = artifactPath url + ".metadata.json";
+                path = writeText "metadata.json" (toJSON {
+                  inherit url;
+                  headers = { };
+                });
+              }
+              ]
+              )
+              (importJSON lockfile).remote
+              ))
+              )
